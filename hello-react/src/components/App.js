6ee9569e2b456efb2660681cc972a998b9c51119
NecessarilyNess <vanessamadu197@gmail.com>
2020-08-29 22:18:53 +0100

    Componentized appointment listing
    - Add indexer to add keys to the appointment elements
    - Add bootstrap code to give appointments a bit more content
    - Loop through data to output individual appointments

--- hello-react/src/components/App.js
+++ hello-react/src/components/App.js
@@ -10,20 +10,38 @@ class App extends Component {
   constructor() {
     super(); //allows you to get information from the parent component.
     this.state = {
-      myName: 'Vanessa'
-    }
+      myAppointments: [],
+      lastIndex : 0
+    };
   }
+
+  componentDidMount () {
+    fetch('./data.json')
+    .then(response => response.json())
+    .then(result => {
+      const apts = result.map(item => {
+        item.aptId = this.state.lastIndex;
+        this.setState({lastIndex: this.state.lastIndex + 1})
+        return item;
+      })
+      this.setState({
+        myAppointments: apts
+      });
+    });
+  }
+
+
   render() {
+
     return (
       <main className="page bg-white" id="petratings">
       <div className="container">
         <div className="row">
           <div className="col-md-12 bg-white">
             <div className="container">
-              {this.state.myName}
               <AddAppointments />
               <SearchAppointments />
-              <ListAppointments />
+              <ListAppointments appointments = {this.state.myAppointments} />
             </div>
           </div>
         </div>
